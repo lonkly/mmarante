cf5f59e33b95b60cce1f204dee4f15c0f4d9e2a0
Vivienne Fosh <vifosh@gmail.com>
2024-07-14 19:29:22 -0400

    wip

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,15 +1,23 @@
-import type { GatsbySSR } from "gatsby"
-import * as React from "react"
+import React from "react";
+import { GatsbySSR } from "gatsby";
 
 export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) => {
-    setHeadComponents([
-        <link
-            rel="preload"
-            href="/fonts/work-sans.var.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-            key="workSansFont"
-        />,
-    ])
-}
+  setHeadComponents([
+    <script
+      key="gtag-js"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-TKMTHR85CF"
+    ></script>,
+    <script
+      key="gtag-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-TKMTHR85CF');
+        `,
+      }}
+    ></script>,
+  ]);
+};
